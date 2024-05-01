9b3bf814cfc337e57e2c828545c89047b6c795b8
resiakact <168393487+resiakact@users.noreply.github.com>
2024-05-02 01:03:53 +0800

    Update educ.js

--- script/educ.js
+++ script/educ.js
@@ -30,7 +30,7 @@ module.exports.run = async function({ api, event, args }) {
   try {
     const { data } = await axios.get(`https://openaikey-x20f.onrender.com/api?prompt=${encodeURIComponent(input)}`);
     let response = data.response;
-    response += "\n\n𝘛𝘩𝘦 𝘣𝘰𝘵 𝘸𝘢𝘴 𝘤𝘳𝘦𝘢𝘵𝘦𝘥 𝘣𝘺 𝘤𝘩𝘶𝘳𝘤𝘩𝘪𝘭𝘭:https://www.facebook.com/Churchill.Dev4100";
+    response += "\n\nthankyouuu";
     api.sendMessage(response, event.threadID, event.messageID);
   } catch (error) {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
