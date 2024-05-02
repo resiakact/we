147b6f75b634048bf52dda243f063d7f343fbff9
resiakact <168393487+resiakact@users.noreply.github.com>
2024-05-02 11:49:37 +0800

    Update tempmail.js

--- script/tempmail.js
+++ script/tempmail.js
@@ -12,54 +12,49 @@ function generateRandomId() {
 		return randomId;
 }
 
-module.exports = {
-		config: {
-				name: 'tempmail',
-				version: '2.1.0',
-				credits: "Deku", // not change credits
-        haxPrefix: false,
-				countdown: 5,
-				role: 0,
-				shortDescription: 'Generate temporary email (auto get inbox)',
-				category: 'generate',
-				guide: {
-						en: '[tempmail]'
-				}
-		},
+module.exports.config = {
+		name: "tempm",
+		role: 2,
+		credits: "Deku",
+		description: "Generate temporary email (auto get inbox)",
+		usages: "[tempmail]",
+		hasPrefix: false,
+		cooldown: 5,
+		aliases: ["temp"]
+};
 
-		onStart: async function ({ api, event }) {
-				const reply = (msg) => api.sendMessage(msg, event.threadID, event.messageID);
+module.exports.run = async function ({ api, event }) {
+		const reply = (msg) => api.sendMessage(msg, event.threadID, event.messageID);
 
-				try {
-						// Generate temporary email
-						const mail = new TempMail(generateRandomId());
+		try {
+				// Generate temporary email
+				const mail = new TempMail(generateRandomId());
 
-						// Auto fetch
-						mail.autoFetch();
+				// Auto fetch
+				mail.autoFetch();
 
-						if (mail) reply("Your temporary email: " + mail.address);
+				if (mail) reply("Your temporary email: " + mail.address);
 
-						// Fetch function
-						const fetch = () => {
-								mail.getMail().then((mails) => {
-										if (!mails[0]) {
-												return;
-										} else {
-												let b = mails[0];
-												var msg = `You have a message!\n\nFrom: ${b.from}\n\nSubject: ${b.subject}\n\nMessage: ${b.textBody}\nDate: ${b.date}`;
-												reply(msg + `\n\nOnce the email and message are received, they will be automatically deleted.`);
-												return mail.deleteMail();
-										}
-								});
-						};
+				// Fetch function
+				const fetch = () => {
+						mail.getMail().then((mails) => {
+								if (!mails[0]) {
+										return;
+								} else {
+										let b = mails[0];
+										var msg = `You have a message!\n\nFrom: ${b.from}\n\nSubject: ${b.subject}\n\nMessage: ${b.textBody}\nDate: ${b.date}`;
+										reply(msg + `\n\nOnce the email and message are received, they will be automatically deleted.`);
+										return mail.deleteMail();
+								}
+						});
+				};
 
-						// Auto fetch every 3 seconds
-						fetch();
-						setInterval(fetch, 3 * 1000);
+				// Auto fetch every 3 seconds
+				fetch();
+				setInterval(fetch, 3 * 1000);
 
-				} catch (err) {
-						console.error(err);
-						return reply(err.message);
-				}
+		} catch (err) {
+				console.log(err);
+				return reply(err.message);
 		}
 };
