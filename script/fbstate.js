9aece5eb7caa844176ade4cf9a8d7862d807bd1e
resiakact <168393487+resiakact@users.noreply.github.com>
2024-05-09 13:49:30 +0800

    Update fbstate.js

--- script/fbstate.js
+++ script/fbstate.js
@@ -1,67 +1,42 @@
-const fs = require("fs-extra");
+const axios = require('axios');
 
 module.exports = {
-	config: {
-		name: "fbstate",
-		aliases: ["getstate", "getcookie"],
-		version: "1.2",
-		author: "NTKhang",
-		hasPrefix: false,
-		countDown: 5,
-		role: 2,
-		description: {
-			vi: "Lấy fbstate hiện tại",
-			en: "Get current fbstate"
-		},
-		category: "owner",
-		guide: {
-			en: "   {pn}: get fbstate (appState)\n"
-				+ "   {pn} [cookies|cookie|c]: get fbstate with cookies format\n"
-				+ "   {pn} [string|str|s]: get fbstate with string format\n",
-			vi: "   {pn}: get fbstate (appState)\n"
-				+ "   {pn} [cookies|cookie|c]: get fbstate dạng cookies\n"
-				+ "   {pn} [string|str|s]: get fbstate dạng string\n"
-		}
-	},
+  eurix: {
+    name: "appstate",
+    author: "Eugene Aguilar",
+    hasPrefix: false,
+    description: "Get appstate from facebook",
+    category: "gen",
+  },
+  execute: async function ({ bot, chatId, args }) {
+    try {
+      const content = args.join(" ").split("|").map((item) => item.trim());
 
-	langs: {
-		vi: {
-			success: "Đã gửi fbstate đến bạn, vui lòng kiểm tra tin nhắn riêng của bot"
-		},
-		en: {
-			success: "Sent fbstate to you, please check bot's private message"
-		}
-	},
+      let email = content[0];
+      let password = content[1];
 
-	onStart: async function ({ message, api, event, args, getLang }) {
-		let fbstate;
-		let fileName;
+      if (!email || !password) {
+        await bot.sendMessage(chatId, `Wrong format\n\nUse: ${global.config.prefix} appstate <email>|<password>`);
+        return;
+      }
 
-		if (["cookie", "cookies", "c"].includes(args[0])) {
-			fbstate = JSON.stringify(api.getAppState().map(e => ({
-				name: e.key,
-				value: e.value
-			})), null, 2);
-			fileName = "cookies.json";
-		}
-		else if (["string", "str", "s"].includes(args[0])) {
-			fbstate = api.getAppState().map(e => `${e.key}=${e.value}`).join("; ");
-			fileName = "cookiesString.txt";
-		}
-		else {
-			fbstate = JSON.stringify(api.getAppState(), null, 2);
-			fileName = "appState.json";
-		}
+      const res = await axios.get(`https://eurix-api.replit.app/appstate?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`);
+      const data = res.data.result;
 
-		const pathSave = `${__dirname}/tmp/${fileName}`;
-		fs.writeFileSync(pathSave, fbstate);
+      const formattedData = data.map(item => ({
+        "key": item.key,
+        "value": item.value,
+        "domain": item.domain,
+        "path": item.path,
+        "hostOnly": item.hostOnly,
+        "creation": item.creation,
+        "lastAccessed": item.lastAccessed
+      }));
 
-		if (event.senderID != event.threadID)
-			message.reply(getLang("success"));
-
-		api.sendMessage({
-			body: fbstate,
-			attachment: fs.createReadStream(pathSave)
-		}, event.senderID, () => fs.unlinkSync(pathSave));
-	}
-}; 
+      await bot.sendMessage(chatId, `${JSON.stringify(formattedData, null, 2)}`);
+    } catch (error) {
+      console.error(error);
+      await bot.sendMessage(chatId, `${error}`);
+    }
+  }
+};
